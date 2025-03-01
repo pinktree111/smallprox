1878795c441df6b11dd503260ba52693100d37ee
pinktree111 <pinkbaobab@proton.me>
2025-03-01 10:58:33 +0100

    Update app.py

--- app.py
+++ app.py
@@ -85,4 +85,4 @@ def proxy_ts():
         return f"Errore durante il download del segmento TS: {str(e)}", 500
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=7860, debug=False)
